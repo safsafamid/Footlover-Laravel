bb5f015d5ccb31b16acdedb3c5e6d58ef4ff48ed
Abdelhamid SAFSAF <abdelhamidsafsaf@gmail.com>
2023-07-10 11:10:05 +0100

    07/07/2023

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,23 @@ class HomeController extends Controller
 
     public function adminHome()
     {
-        $ip = '162.159.24.227'; /* Static IP address */
+        function getIPAddress() {  
+            //whether ip is from the share internet  
+             if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+                        $ip = $_SERVER['HTTP_CLIENT_IP'];  
+                }  
+            //whether ip is from the proxy  
+            elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+             }  
+        //whether ip is from the remote address  
+            else{  
+                     $ip = $_SERVER['REMOTE_ADDR'];  
+             }  
+             return $ip;  
+        }  
+        $ip = getIPAddress();  
+
         $currentUserInfo = Location::get($ip);
 
         return view('back.index', compact('currentUserInfo'));
